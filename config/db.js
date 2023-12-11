ff608892c8be13acbba86ef3e7402ba50b1859c9
Nadia <nadiahosisy@gmail.com>
2023-12-11 13:21:17 +0200

    backup for backend

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(
+      `MongoDB Connected: ${conn.connection.host}`.yellow.underline.bold
+    );
+  } catch (err) {
+    console.log(`${err}`.red);
+  }
+};
+
+module.exports = connectDB;
